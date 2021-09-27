942400595b1cc808c2ded8ff87fdfa5a14506dd8
innerNULL <lyt697@outlook.com>
2021-09-27 10:09:47 +0000

    fix

--- pylocatable/templates.py
+++ pylocatable/templates.py
@@ -17,8 +17,17 @@ RUN bash ./init.sh
 """
 
 
-DOCKER_RUN_TEMP = """
+DOCKER_RUN_TEMP_BAK = """
 sudo docker run -it --privileged \
+        --name {container_name} \
+        -v {mounting_path}:/workspace \
+        --net=host \
+        {image} \
+        bash -c '{cmd}'
+"""
+
+DOCKER_RUN_TEMP = """
+sudo docker run -i --privileged \
         --name {container_name} \
         -v {mounting_path}:/workspace \
         --net=host \
